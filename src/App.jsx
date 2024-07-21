c74851490cbfb63d1531d834d956fc5f4ebc8df1
Atakan Temizkan <atikotemizkan@gmail.com>
2024-07-21 20:45:50 +0300

    fixed error in committing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import { useState } from "react";
 import "./App.css";
 import TopHeader from "./TopHeader";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <>
       <TopHeader />
-      <aside></aside>
+      <Sidebar />
     </>
   );
 }
---
+++ src/Sidebar.jsx
@@ -0,0 +1,7 @@
+export default function Sidebar() {
+  return (
+    <aside>
+      <h2>Hi</h2>
+    </aside>
+  );
+}

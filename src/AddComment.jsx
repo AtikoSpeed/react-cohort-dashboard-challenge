314fa1999b860f0f3d85f4dc70471a0c071cfa2f
Atakan Temizkan <atikotemizkan@gmail.com>
2024-07-24 20:35:44 +0300

    added comment field

--- src/AddComment.jsx
+++ src/AddComment.jsx
@@ -5,6 +5,16 @@ export default function AddComment() {
   return (
     <div className="add-comment">
       <MainPropic />
+      <div className="comment-field">
+        <input type="text" placeholder="Add a comment..." />
+        <div className="comment-button-container">
+          <img
+            src="src/assets/send.svg"
+            alt="Send"
+            className="comment-button"
+          />
+        </div>
+      </div>
     </div>
   );
 }
